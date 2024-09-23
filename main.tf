ca9fd716aa699c2db8ee005ef119db65d48dd33c
Henryk Rychlik <henryk.rychlik@hashicorp.com>
2024-09-23 17:18:43 -0500

    aaa

--- main.tf
+++ main.tf
@@ -1,1 +1,12 @@
-resource "null_resource" "null" {}
+resource "null_resource" "cluster" {}
+
+data "tfe_organization" "test-organization" {
+  name  = "hrychlik"
+#  email = "admin@company.com"
+}
+
+resource "tfe_workspace" "test" {
+  name         = "my-workspace-name"
+  organization = data.tfe_organization.test-organization.name
+  tag_names    = ["test", "app"]
+}
